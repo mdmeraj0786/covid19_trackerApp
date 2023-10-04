56101ec4a5c8ef7d48f4e5035b5dfefa57128c9e
mdmeraj0786 <mdmeraj787884@gmail.com>
2023-10-05 00:51:18 +0530

    first commit

--- lib/View/world_states.dart
+++ lib/View/world_states.dart
@@ -10,11 +10,11 @@ class WorldStates extends StatefulWidget {
   State<WorldStates> createState() => _WorldStatesState();
 }
 
-class _WorldStatesState extends State<WorldStates> with TickerProviderStateMixin{
-
-  late final AnimationController _controller = AnimationController(
-      duration: const Duration(seconds: 3),
-      vsync: this)..repeat();
+class _WorldStatesState extends State<WorldStates>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: const Duration(seconds: 3), vsync: this)
+        ..repeat();
 
   @override
   void dispose() {
@@ -37,40 +37,45 @@ class _WorldStatesState extends State<WorldStates> with TickerProviderStateMixin
           padding: const EdgeInsets.all(15.0),
           child: Column(
             children: [
-              SizedBox(height: MediaQuery.of(context).size.height * .01,),
-               PieChart(
-                  dataMap: const {
-                    "Total": 20,
-                    "Recovered": 15,
-                    "Deaths": 5,
-                  },
-                 chartRadius: MediaQuery.of(context).size.width / 2.3,
-                 legendOptions: const LegendOptions(
-                   legendPosition: LegendPosition.left,
-                 ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .01,
+              ),
+              PieChart(
+                dataMap: const {
+                  "Total": 20,
+                  "Recovered": 15,
+                  "Deaths": 5,
+                },
+                chartRadius: MediaQuery.of(context).size.width / 2.3,
+                legendOptions: const LegendOptions(
+                  legendPosition: LegendPosition.left,
+                ),
                 animationDuration: const Duration(milliseconds: 1200),
                 chartType: ChartType.ring,
                 colorList: colorList,
               ),
-               Padding(
-                 padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * .06),
-                 child: Card(
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: MediaQuery.of(context).size.height * .06),
+                child: Card(
                   child: Column(
                     children: [
-                      ReusableRow(title: 'Total', value: '200',),
+                      ReusableRow(
+                        title: 'Total',
+                        value: '200',
+                      ),
                       ReusableRow(title: "Total", value: '300'),
                       ReusableRow(title: "Total", value: '300'),
                       ReusableRow(title: "Total", value: '300'),
                     ],
                   ),
+                ),
               ),
-               ),
               Container(
                 height: 50,
                 decoration: BoxDecoration(
-                  color: const Color(0xff1aa260),
-                  borderRadius: BorderRadius.circular(10)
-                ),
+                    color: const Color(0xff1aa260),
+                    borderRadius: BorderRadius.circular(10)),
                 child: const Center(
                   child: Text("Track Countries"),
                 ),
@@ -90,7 +95,7 @@ class ReusableRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 10,left: 10, right: 10, bottom: 5),
+      padding: const EdgeInsets.only(top: 10, left: 10, right: 10, bottom: 5),
       child: Column(
         children: [
           Row(
@@ -100,7 +105,9 @@ class ReusableRow extends StatelessWidget {
               Text(value),
             ],
           ),
-          const SizedBox(height: 5,)
+          const SizedBox(
+            height: 5,
+          )
         ],
       ),
     );
